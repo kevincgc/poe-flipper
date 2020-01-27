20c7b1a4f4e9ca8933a8734a8335d5bc71544751
kevincgc <kevincgc@gmail.com>
2020-01-26 16:35:27 -0800

    added site link constants

--- src/Constants.java
+++ src/Constants.java
@@ -1,32 +1,32 @@
 
 public class Constants {
 	//wisdom
-	public static final String BUY_WIS = "";
-	public static final String SELL_WIS = "";
+	public static final String BUY_WIS = "https://www.pathofexile.com/trade/exchange/Metamorph/JBVail";
+	public static final String SELL_WIS = "https://www.pathofexile.com/trade/exchange/Metamorph/3qpGU5";
 	//portal
-	public static final String BUY_PORT = "";
-	public static final String SELL_PORT = "";
+	public static final String BUY_PORT = "https://www.pathofexile.com/trade/exchange/Metamorph/powWf0";
+	public static final String SELL_PORT = "https://www.pathofexile.com/trade/exchange/Metamorph/bE5GcL";
 	//transmutation
-	public static final String BUY_TRA = "";
-	public static final String SELL_TRA = "";
+	public static final String BUY_TRA = "https://www.pathofexile.com/trade/exchange/Metamorph/zbqvI4";
+	public static final String SELL_TRA = "https://www.pathofexile.com/trade/exchange/Metamorph/PrBMcL";
 	//augmentation
-	public static final String BUY_AUG = "";
-	public static final String SELL_AUG = "";
+	public static final String BUY_AUG = "https://www.pathofexile.com/trade/exchange/Metamorph/dZk3cJ";
+	public static final String SELL_AUG = "https://www.pathofexile.com/trade/exchange/Metamorph/5M2osa";
 	
 	//whetstone
-	public static final String BUY_WHE = "";
-	public static final String SELL_WHE = "";
+	public static final String BUY_WHE = "https://www.pathofexile.com/trade/exchange/Metamorph/VLywup";
+	public static final String SELL_WHE = "https://www.pathofexile.com/trade/exchange/Metamorph/KBWPi5";
 	//armorer
-	public static final String BUY_SCR = "";
-	public static final String SELL_SCR = "";
+	public static final String BUY_SCR = "https://www.pathofexile.com/trade/exchange/Metamorph/z942f4";
+	public static final String SELL_SCR = "https://www.pathofexile.com/trade/exchange/Metamorph/v0eDfE";
 	//blessed
-	public static final String BUY_BLESSED = "";
-	public static final String SELL_BLESSED = "";
+	public static final String BUY_BLESSED = "https://www.pathofexile.com/trade/exchange/Metamorph/5n8nta";
+	public static final String SELL_BLESSED = "https://www.pathofexile.com/trade/exchange/Metamorph/Q2Q7Cw";
 	//regal
-	public static final String BUY_REGAL = "";
-	public static final String SELL_REGAL = "";
+	public static final String BUY_REGAL = "https://www.pathofexile.com/trade/exchange/Metamorph/YpagsY";
+	public static final String SELL_REGAL = "https://www.pathofexile.com/trade/exchange/Metamorph/EBBVC5";
 	//perandus
-	public static final String BUY_P = "";
-	public static final String SELL_P = "";
+	public static final String BUY_P = "https://www.pathofexile.com/trade/exchange/Metamorph/243DIk";
+	public static final String SELL_P = "https://www.pathofexile.com/trade/exchange/Metamorph/YpLRsY";
 	
 }
