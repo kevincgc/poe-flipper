00d78de6e6e183835bc37bc3e999f941b70de6f5
kevincgc <kevincgc@gmail.com>
2020-01-26 16:07:52 -0800

    initial commit

---
+++ src/ChromeHeadlessTest.java
@@ -0,0 +1,62 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import com.google.common.io.Files;
+
+public class ChromeHeadlessTest {
+	private static String userName = "";
+	private static String password = "";
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		String chromeDriverPath = "./bin/chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080", "--ignore-certificate-errors",
+				"--silent");
+		WebDriver driver = new ChromeDriver(options);
+		print("======================running..");
+
+		// Get the login page
+		driver.get("https://www.pathofexile.com/trade/exchange/Metamorph/dZk3cJ");
+		Thread.sleep(1200);
+
+//      // Search for username / password input and fill the inputs
+//      driver.findElement(By.xpath("//input[@name='acct']")).sendKeys(userName);
+//      driver.findElement(By.xpath("//input[@type='password']")).sendKeys(password);
+//        
+//      // Locate the login button and click on it
+//      driver.findElement(By.xpath("//input[@value='login']")).click();
+//        
+//      if(driver.getCurrentUrl().equals("https://news.ycombinator.com/login")){
+//       	System.out.println("Incorrect credentials");
+//       	driver.quit();
+//       	System.exit(1);
+//      }else{
+//       	System.out.println("Successfuly logged in");
+//      }
+//      
+		
+		
+		print(driver.findElement(By.xpath("//*[@id=\"trade\"]/div[6]/div[2]/div[1]/div[1]/div/div[2]/span[3]/span[1]")).getText());
+		// Take a screenshot of the current page
+		File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		Files.copy(screenshot, new File("screenshot.png"));
+
+		// Logout
+		// driver.findElement(By.id("logout")).click();
+		driver.quit();
+		print("======================done..");
+	}
+
+	public static void print(String string) {
+		System.out.println(string);
+	}
+}
---
+++ src/Constants.java
@@ -0,0 +1,32 @@
+
+public class Constants {
+	//wisdom
+	public static final String BUY_WIS = "";
+	public static final String SELL_WIS = "";
+	//portal
+	public static final String BUY_PORT = "";
+	public static final String SELL_PORT = "";
+	//transmutation
+	public static final String BUY_TRA = "";
+	public static final String SELL_TRA = "";
+	//augmentation
+	public static final String BUY_AUG = "";
+	public static final String SELL_AUG = "";
+	
+	//whetstone
+	public static final String BUY_WHE = "";
+	public static final String SELL_WHE = "";
+	//armorer
+	public static final String BUY_SCR = "";
+	public static final String SELL_SCR = "";
+	//blessed
+	public static final String BUY_BLESSED = "";
+	public static final String SELL_BLESSED = "";
+	//regal
+	public static final String BUY_REGAL = "";
+	public static final String SELL_REGAL = "";
+	//perandus
+	public static final String BUY_P = "";
+	public static final String SELL_P = "";
+	
+}
